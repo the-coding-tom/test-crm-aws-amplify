2a40e2d9df6c166cee5a759cf81de1266cbaca72
nicholaschun <nicholaschunrayne@gmail.com>
2019-10-08 12:58:49 +0000

    Revert "added resources"
    This reverts commit 9a98350b7fb3fd7529f612bd3d571d984dc9b3a9.

--- pages/_space/resources/index.vue
+++ pages/_space/resources/index.vue
@@ -1,94 +1,12 @@
 <template>
   <div>
-    <base-header
-      class="pb-6"
-      type="">
-      <div class="d-flex justify-content-between py-4">
-        <MainTitle
-          title="Resources"/>
-        <nuxt-link
-          :to="{ name: 'space-resources-add'}"
-          class="btn btn-primary"
-        >Add Resource</nuxt-link>
-      </div>
-    </base-header>
-
-    <!--Charts-->
-    <div class="container-fluid mt--6">
-      <div class="row equal">
-        <div 
-          v-for="rooms in allRooms" 
-          :key="rooms.id"
-          class="col-md-4">
-          <card class="sh-book-res">
-            <div class="m-n25 img-wrap">
-              <img 
-                :src="rooms.photo"
-                class="img-fluid"
-                alt="">
-              <badge>San Francisco</badge>
-            </div>
-
-            <div class="mr-t-50">
-              <div class="d-flex justify-content-between mr-b-20">
-                <h3>{{ rooms.name }}</h3>
-                <h3 class="text-muted">{{ rooms.price_per_hour }} USD/hr</h3>
-              </div>
-              <p class="text-muted">{{ rooms.description }}</p>
-            </div>
-
-            <div class="mr-t-30">
-              <p>24 hrs cancellation notice</p>
-              <p>Available from: Mondays - Fridays, 8:00AM - 5:00PM</p>
-            </div>
-
-            <div slot="footer">
-              <div class="d-flex justify-content-between">
-                <nuxt-link :to="{ name: 'space-resources-id', params: { id: rooms.id }}">
-                  <i class="ti-pencil" /> Edit Resource
-                </nuxt-link>
-                <a 
-                  href="" 
-                  class="text-danger"><i class="ti-trash"/> Delete Resource</a>
-              </div>
-            </div>
-          </card>
-        </div>
-      </div>
-    </div>
-
+    <h1>resources goes here</h1>
   </div>
 </template>
-<script>
-import { mapState } from 'vuex'
-import TopWidget from '~/components/shack/TopWidget.vue'
-import Activity from '~/components/shack/Activity.vue'
-import MainTitle from '~/components/shack/MainTitle.vue'
-import SectionTitle from '~/components/shack/SectionTitle.vue'
-import EmptyActivity from '~/components/shack/EmptyActivity.vue'
-import FeedTable from '~/components/shack/FeedTable.vue'
-import Booking from '~/components/shack/Booking.vue'
 
-export default {
-  layout: 'ShackDash',
-  components: {
-    Activity,
-    TopWidget,
-    Booking,
-    FeedTable,
-    EmptyActivity,
-    SectionTitle,
-    MainTitle
-  },
-  computed: {
-    ...mapState({
-      allRooms: state => state.resources.rooms.data
-    })
-  },
-  created() {
-    this.$store.dispatch('resources/getAllRooms', { vm: this })
-  }
-}
+<script>
+export default {}
 </script>
+
 <style>
 </style>
--- plugins/services/resource.js
+++ plugins/services/resource.js
@@ -1,22 +1,7 @@
 export default function(ctx, inject) {
-  const subdomain = ctx.route.params.space
-  const baseUrl = `/${subdomain}/admin/rooms`
-
   const Resource = {
     getAllRooms: () => {
-      return ctx.$axios.get(`${baseUrl}`)
-    },
-    getARoom: id => {
-      return ctx.$axios.get(`${baseUrl}/${id}`)
-    },
-    createRoom: payload => {
-      return ctx.$axios.post(`${baseUrl}`, payload)
-    },
-    updateRoom: (id, payload) => {
-      return ctx.$axios.patch(`${baseUrl}/${id}`, payload)
-    },
-    deleteRoom: () => {
-      return ctx.$axios.delete(`${baseUrl}/${id}`)
+      console.log('>>>>>>>>>>>>>>>>>>>>')
     }
   }
   ;(ctx.$resource = Resource), inject('resource', Resource)
