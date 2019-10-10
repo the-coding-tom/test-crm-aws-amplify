0d579d366721fdfbb42ceddc913c806c6ac77c5f
Abdul-Harisu Inusah <inusahharis@gmail.com>
2019-10-10 10:02:57 +0000

    make subdomain dynamic

--- plugins/services/event.js
+++ plugins/services/event.js
@@ -1,64 +1,69 @@
 export default function(ctx, inject) {
+  const subdomain = ctx.route.params.space
+
   const Event = {
     addEvent: payload => {
-      return ctx.$axios.$post('/shack15/events', payload)
+      return ctx.$axios.$post(`/${subdomain}/events`, payload)
     },
     deleteEvent: eventId => {
-      return ctx.$axios.$delete(`/shack15/events/${eventId}`)
+      return ctx.$axios.$delete(`/${subdomain}/events/${eventId}`)
     },
     updateEvent: (id, payload) => {
-      return ctx.$axios.put(`/shack15/events/${id}`, payload)
+      return ctx.$axios.put(`/${subdomain}/events/${id}`, payload)
     },
     getEvent: eventId => {
-      return ctx.$axios.$get(`/shack15/events/${eventId}`)
+      return ctx.$axios.$get(`/${subdomain}/events/${eventId}`)
     },
     getEvents() {
-      return ctx.$axios.$get('/shack15/events')
+      return ctx.$axios.$get(`/${subdomain}/events`)
     },
     addCategory(payload) {
-      return ctx.$axios.$post('/shack15/event-categories', payload)
+      return ctx.$axios.$post(`/${subdomain}/event-categories`, payload)
     },
     updateCategory(eventId, payload) {
-      return ctx.$axios.$patch(`/shack15/event-categories/${eventId}`, payload)
+      return ctx.$axios.$patch(
+        `/${subdomain}/event-categories/${eventId}`,
+        payload
+      )
     },
     getEventCategories() {
-      return ctx.$axios.$get('/shack15/event-categories')
+      return ctx.$axios.$get(`/${subdomain}/event-categories`)
     },
     deleteCategory(eventId) {
-      return ctx.$axios.delete(`/shack15/event-categories/${eventId}`)
+      return ctx.$axios.delete(`/${subdomain}/event-categories/${eventId}`)
     },
     getRooms() {
-      return ctx.$axios.get('/shack15/admin/rooms')
+      return ctx.$axios.get(`/${subdomain}/admin/rooms`)
     },
     getAttendees(eventId = null, link = null) {
       if (!eventId) {
         return ctx.$axios.get(link)
       }
 
-      return ctx.$axios.get(`/shack15/events/${eventId}/attendees`)
+      return ctx.$axios.get(`/${subdomain}/events/${eventId}/attendees`)
     },
     purchaseTicketByAdmin(eventId, payload) {
       return ctx.$axios.post(
-        `/shack15/events/${eventId}/purchase-ticket`,
+        `/${subdomain}/events/${eventId}/purchase-ticket`,
         payload
       )
     },
     cancelTicket(eventId, payload) {
       return ctx.$axios.delete(
-        `/shack15/events/${eventId}/cancel-ticket`,
+        `/${subdomain}/events/${eventId}/cancel-ticket`,
         payload
       )
     },
     getMemberships() {
-      return ctx.$axios.get('/shack15/memberships')
+      return ctx.$axios.get(`/${subdomain}/memberships`)
     },
     getEventsByDate(startDate, endDate) {
       return ctx.$axios.get(
-        `/shack15/events?filter[starts_between]=${startDate},${endDate}`
+        `/${subdomain}/events?filter[starts_between]=${startDate},${endDate}`
       )
     },
     deleteEvent(eventId) {
-      return ctx.$axios.delete(`/shack15/events/${eventId}/`)
+      return ctx.$axios.delete(`/${subdomain}/events/${eventId}/`)
     }
   }
   ;(ctx.$event = Event), inject('event', Event)
