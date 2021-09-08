dbc1d5198f114731f5b5b6a5d7701c7ec5104d74
iamsirmike <asamoahmichael77@gmail.com>
2021-09-08 12:47:11 +0000

    add subscription to response

--- pages/_space/directory/index.vue
+++ pages/_space/directory/index.vue
@@ -108,7 +108,7 @@ export default {
   name: 'Directory',
   layout: 'ShackDash',
   async asyncData({ store, $membership, route }) {
-    const link = `filter[status]=accepted&include=profile,primaryPlan&page=${
+    const link = `filter[status]=accepted&include=profile,subscriptions,primaryPlan&page=${
       route.query.page
     }`
     return await $membership
@@ -204,7 +204,7 @@ export default {
     },
     filterBy() {
       this.loading = !this.loading
-      const link = `filter[status]=accepted&include=profile,primaryPlan&filter[prefix_type]=${
+      const link = `filter[status]=accepted&include=profile,subscriptions,primaryPlan&filter[prefix_type]=${
         this.filter
       }`
       this.$membership
