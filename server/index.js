371086350725f4469cc0a3fc25a41fcdc007d4a6
Thomas Kensah <tom@Thomass-MacBook-Pro.local>
2023-10-10 09:20:57 +0000

    debuging issue with running CRM

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+console.log("Node.js version:", process.version);
 const consola = require('consola')
 const { Nuxt, Builder } = require('nuxt')
 const app = express()
