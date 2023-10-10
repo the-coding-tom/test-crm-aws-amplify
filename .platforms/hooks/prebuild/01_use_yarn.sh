6f5baca79540f5b0102b5658ccffc7cc2ea1132a
Thomas Kensah <tom@Thomass-MacBook-Pro.local>
2023-10-10 16:25:02 +0000

    debuging issue on CRM - yarn

--- .platforms/hooks/prebuild/01_use_yarn.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Output the Yarn version for debugging purposes
-echo "Using Yarn version: $(yarn --version)"
-
-# Set the PATH to include Yarn binary
-export PATH="$PATH:$(yarn global bin)"
